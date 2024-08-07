ec2e1e725f6d0b0fc96f9ad9a0678c9ba6a44dfc
23036 <23036@supnum.mr>
2024-08-07 15:11:57 +0000

    environnement

--- gestion_stage/stage_moov/admin.py
+++ gestion_stage/stage_moov/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Utilisateur,Candidats,Service,Sujet_stage,Demandes,Document,Affectation,Evaluation,Attestation,Notification
 
 # Register your models here.
+admin.site.register(Utilisateur)
+admin.site.register(Candidats)
+admin.site.register(Demandes)
+admin.site.register(Document)
+admin.site.register(Service)
+admin.site.register(Sujet_stage)
+admin.site.register(Affectation)
+admin.site.register(Attestation)
+admin.site.register(Evaluation)
+admin.site.register(Notification)
